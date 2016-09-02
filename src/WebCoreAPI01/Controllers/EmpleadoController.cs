73d218ae354c64d2548e2a729ba8b121c5d33090
pcuadros_dev <pcuadros_dev@TIDE-12OP-12>
2016-09-02 16:01:09 -0500

    Add project files.

---
+++ src/WebCoreAPI01/Controllers/EmpleadoController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebCoreAPI01.Model;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebCoreAPI01.Controllers
+{
+    [Route("api/[controller]")]
+    public class EmpleadoController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<Empleado> Get()
+        {
+            var empleados = new List<Empleado>
+           {
+               new Empleado {IDEmpleado = 1,Nombres = "Jalpesh",Apellidos = "Vadgama",Designacion = "Technical Architect"},
+               new Empleado {IDEmpleado = 2,Nombres = "Vishal",Apellidos = "Vadgama",Designacion = "Technical Lead"}
+           };
+           return empleados;
+        }
+
+        // GET api/values/5
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        //// POST api/values
+        //[HttpPost]
+        //public void Post([FromBody]string value)
+        //{
+        //}
+
+        //// PUT api/values/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
+
+        //// DELETE api/values/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
---
+++ src/WebCoreAPI01/Model/Empleado.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebCoreAPI01.Model
+{
+    public class Empleado
+    {
+        public int IDEmpleado { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public string Designacion { get; set; }
+    }
+}
